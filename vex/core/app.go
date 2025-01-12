3f63acad25d296447a614ccdcabe6315073b336b
JXSnack <jx.snack@gmail.com>
2025-01-12 20:42:47 +0100

    open win and render

--- vex/core/app.go
+++ vex/core/app.go
@@ -1,6 +1,9 @@
 package core
 
-import "github.com/snackbag/vex-go/vex/widgets"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+	"github.com/snackbag/vex-go/vex/widgets"
+)
 
 type VApp struct {
 	x      int
@@ -23,9 +26,38 @@ func (app *VApp) Hide() {
 }
 
 func (app *VApp) SetVisibility(visibility bool) {
-	app.visibility = visibility
+	if visibility == !app.visibility {
+		app.visibility = visibility
+
+		if visibility {
+			app.startRaylibRendering()
+		} else {
+			rl.CloseWindow()
+		}
+	}
+}
+
+func (app *VApp) startRaylibRendering() {
+	rl.SetTraceLogLevel(rl.LogError)
+	rl.InitWindow(int32(app.width), int32(app.height), app.title)
+
+	rl.SetTargetFPS(60)
+	for !rl.WindowShouldClose() && app.visibility {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.White)
+
+		for _, element := range app.widgets {
+			element.Render()
+		}
+
+		rl.EndDrawing()
+	}
+
+	if rl.WindowShouldClose() || !app.visibility {
+		rl.CloseWindow()
+	}
 }
 
 func NewApp(title string) *VApp {
-	return &VApp{title: title, x: 0, y: 0, width: 640, height: 480}
+	return &VApp{title: title, x: 0, y: 0, width: 640, height: 480, visibility: false}
 }
